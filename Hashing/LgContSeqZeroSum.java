eccac933db74c44c1fa2bfdc83eee7acf45952f2
noobmaster69jr <66981286+noobmaster69jr@users.noreply.github.com>
2023-04-08 14:55:46 +0530

    Update LgContSeqZeroSum.java

--- Hashing/LgContSeqZeroSum.java
+++ Hashing/LgContSeqZeroSum.java
@@ -2,29 +2,6 @@ import java.util.HashMap;
 
 /*find the length of the largest continuous sequence in the array which sums to zero*/
 
-//public class LgContSeqZeroSum {
-//	static void LongFunc(int[] arr, int n) {
-//		int[] prefixSum = new int[n];
-//		prefixSum[0] = arr[0];
-//		for (int i = 1; i < n; i++) {
-//			prefixSum[i] = prefixSum[i - 1] + arr[i];
-//		}
-//		HashMap<Integer, Integer> h = new HashMap<>();
-//		h.put(0, 0);
-//		int maxLength = 0;
-//		for (int i = 0; i < n; i++) {
-//			if (h.containsKey(prefixSum[i])) {
-//				int j = h.get(prefixSum[i]);
-//				if (maxLength < i - j + 1) {
-//					maxLength = i - j + 1;
-//				}
-//			} else {
-//				h.put(prefixSum[i], i + 1);
-//			}
-//		}
-//		System.out.print(maxLength);
-//	}
-
 public class LgContSeqZeroSum {
 	static void LongFunc(int[] arr, int n) {
 		int preSum = 0;
