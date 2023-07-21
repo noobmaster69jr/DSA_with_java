030de7b943d69741b139c7ace3a2a919d79cc264
noobmaster69jr <66981286+noobmaster69jr@users.noreply.github.com>
2023-07-21 21:45:10 +0530

    are 2 trees mirror

--- Tree/BinaryTreeDemo.java
+++ Tree/BinaryTreeDemo.java
@@ -1,6 +1,20 @@
 package binarytree;
 
 public class BinaryTreeDemo {
+
+	public static boolean areMirrorTrees(Node a_root, Node b_root) {
+	   if(a_root == null && b_root == null) {
+	    return true;
+	   }
+           if(a_root == null || b_root == null) {
+            return false;
+           }
+         
+           return a_root.data == b_root.data &&
+                areMirrorTrees(a_root.left, b_root.right) &&
+                areMirrorTrees(a_root.right, b_root.left);
+        }
+     
 	public static void main(String[] args) {
 		BinaryTree bt = new BinaryTree();
 		bt.print();
